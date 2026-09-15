47ab746cc0889db179d62bf8fb99d50dd819606d
Fayyy66 <Fayyy66@users.noreply.github.com>
2026-09-15 15:05:03 +1200

    Add initial database setup scripts

---
+++ awesome_project/setup/production.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE `awesome_db`;
+
+USE `awesome_db`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB
+  DEFAULT CHARSET=utf8mb4
+  COLLATE=utf8mb4_unicode_ci;
